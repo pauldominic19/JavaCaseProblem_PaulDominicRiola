2fd428ba9d0b705e06d1aa120910067aeb94b35d
pauldominic19 <riolapauldominic96@gmail.com>
2025-10-17 12:33:12 +0800

    Uploaded Java case problem and output screenshot

---
+++ OOP-PIT/DiffCase.java
@@ -0,0 +1,14 @@
+public class DiffCase {
+    public static void main(String[] args) {
+        
+        System.out.println("Name: Paul Dominic Riola");
+        System.out.println("ID Number: 2024304226");
+        System.out.println("----------------------------------");
+
+        int num1 = 50;
+        int num2 = 20;
+        int diff = num1 - num2;
+
+        System.out.println("Result: The difference of " + num1 + " and " + num2 + " is " + diff);
+    }
+}
